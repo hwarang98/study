f6c8b30a45ce5e42dde8db16975a76778f6fa5e9
Hwa Rang <snow2271@gmail.com>
2021-10-01 15:42:22 +0900

    생활코딩 리액트 복습(key)

--- react_review/components/Page.js
+++ react_review/components/Page.js
@@ -3,12 +3,21 @@ import React,{ Component } from 'react';
 
 class Page extends Component{
     render() {
+        // key
+        let list = [];
+        let data = this.props.data
+        let i = 0;
+        while(i < data.length){
+            // key라는 것은 <li>의 각각의 목록을 다른 것들과 구분 할 수 있는 식별자를 적어주면된다.
+            // 우리가 필요해서 쓰는것이 아닌 react가 필요해서 우리에게 요청하는것
+            // key를 적지않으면 console에 에러가 뜬다.
+            list.push(<li key={data[i].id}><a href={"/content/" + data[i].id}>{data[i].title}</a></li>)
+            i++
+        }
     return(
         <nav>
         <ul>
-            <li><a href="1.html">HTML</a></li>
-            <li><a href="1.html">CSS</a></li>
-            <li><a href="1.html">JavaScript</a></li>
+            {list}
         </ul>
     </nav>
     );
