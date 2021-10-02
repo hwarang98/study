ef72e201a70de3a1ea58ac3ee4fcc7114e66ce63
Hwa Rang <snow2271@gmail.com>
2021-10-02 14:31:21 +0900

    생활코딩 리액트 복습(이벤트설치, 이벤트에서 state변경하기, 컴포넌트 이벤트 만들기)

--- react_review/App.js
+++ react_review/App.js
@@ -11,9 +11,11 @@ class App extends Component{
     super(props);
     // TODO: 컴포넌트가 실행될때 constructor라는 함수가있다면 제일먼저 실행되고 초기화를 담당한다.
     this.state = {
+      mode:"welcome",
       subject:{title: "WEB", sub: "world wide web!"},
+      welcome: {title: "Welcome", desc: "Hello, React!"},
       contents:[
-        {id:1, title: "HTML", desc:"HTML is HyperText ..."},
+        {id:1, title: "HTML", desc:"HTML is for information"},
         {id:2, title: "CSS", desc:"CSS is for design"},
         {id:3, title: "JavaScript", desc:"JavaScript is for interactive"},
         {id:4, title: "phython", desc:"파이썬 공부해보자"},
@@ -22,7 +24,16 @@ class App extends Component{
   }
   // **state**
 
-  render() {  
+  render() {
+    let _title, _desc = null;
+    if(this.state.mode === 'welcome'){
+      _title = this.state.welcome.title;
+      _desc = this.state.welcome.desc;
+    }
+    else if(this.state.mode === 'read'){
+      _title = this.state.contents[0].title;
+      _desc = this.state.contents[0].desc;
+    }
     return(
       // !!컴포넌트를 만들때에는 반드시 하나의 최상위 태그로 시작해야하며 존재해야한다.<div>
       <div className="App">
@@ -31,11 +42,27 @@ class App extends Component{
           Article 컴포넌트의 title, desc의 " "안의 내용이 바뛴다. */}
         <Subject
           title={this.state.subject.title}
-          sub={this.state.subject.sub}>
+          sub={this.state.subject.sub}
+          onChangePage = {()=>{
+            this.setState({mode:'welcome'})
+            alert("ㅎ2")
+          }}
+          >
         </Subject>
+        {/* <header> 
+          <h1><a href="/" onClick={ (e)=> {
+            console.log(e);
+            e.preventDefault(); // 기본적은 동작 금지
+            this.state.mode = 'welcome' // arrow function 사용시 자동으로 바인딩 해줌
+            this.setState({
+              mode:'welcome'
+            })
+          }}>{this.state.subject.title}</a></h1>
+        {this.state.subject.sub}
+        </header> */}
         {/* key */}
         <Page data={this.state.contents}></Page>
-        <Article title="HTML" desc="HTML is HyperText Markup Language."></Article>
+        <Article title={_title} desc={_desc}></Article>
       </div>
     )
   }
