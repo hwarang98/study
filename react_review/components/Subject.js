529b3f16384afa4d1ab4ab0538837f33dcebaa69
Hwa Rang <snow2271@gmail.com>
2021-10-02 14:32:23 +0900

    생활코딩 리액트 복습(컴포넌트 이벤트 만들기)

--- react_review/components/Subject.js
+++ react_review/components/Subject.js
@@ -3,13 +3,19 @@ import React,{ Component } from 'react';
 class Subject extends Component{
     render() {
     return(
-        // !!컴포넌트를 만들때에는 반드시 하나의 최상위 태그로 시작해야하며 존재해야한다.<header>
         <header>
-        <h1>{this.props.title}</h1>
+            {/* prps를 사용하여 App.js의 App 컴포넌트안에 있는 Subject 컴포넌트의
+                title의 값을 더욱 쉽게 변경 가능하게 만들어줌. this.props.title의
+                부분에서 title을 동일하게 바꿔주면 된다.(sub도 마찬가지) */}
+        <h1><a href="/" onClick={(e)=>{
+            e.preventDefault(); 
+            this.props.onChangePage();
+        }}>{this.props.title}</a></h1>
         {this.props.sub}
         </header>
     );
     }
 }
 
+// Subject라는 파일을 외부에서 사용할수있게 허용해주는 코드
 export default Subject;
