17afa03dcaa27b417944fa299c13847cea7c802a
Hwa Rang <snow2271@gmail.com>
2021-10-10 22:37:04 +0900

    생활코딩 React review 컴포넌트 이벤트 만들기

--- react_review/components/Page.js
+++ react_review/components/Page.js
@@ -10,8 +10,17 @@ class Page extends Component{
         while(i < data.length){
             // key라는 것은 <li>의 각각의 목록을 다른 것들과 구분 할 수 있는 식별자를 적어주면된다.
             // 우리가 필요해서 쓰는것이 아닌 react가 필요해서 우리에게 요청하는것
-            // key를 적지않으면 console에 에러가 뜬다.
-            list.push(<li key={data[i].id}><a href={"/content/" + data[i].id}>{data[i].title}</a></li>)
+            // key를 적지않 으면 console에 에러가 뜬다.
+            list.push(
+                <li key={data[i].id}>
+                    <a href={"/content/" + data[i].id}
+                    data-id = {data[i].id}
+                    onClick = {(e) => {
+                        e.preventDefault();
+                        this.props.onChangePage(e.target.dataset.id);
+                    }}>
+                    {data[i].title}</a>
+                    </li>)
             i++
         }
     return(
