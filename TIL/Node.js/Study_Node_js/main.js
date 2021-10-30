31f1baca3c853d0b5d71cc8509067c9f563b05d1
Hwa Rang <snow2271@gmail.com>
2021-10-31 03:10:47 +0900

    fs.readFile을 통해 본문내용 불러오기(Read)

--- TIL/Node.js/Study_Node_js/main.js
+++ TIL/Node.js/Study_Node_js/main.js
@@ -2,41 +2,70 @@ let http = require('http');
 let fs = require('fs');
 let url = require('url');
 
-let app = http.createServer(function(request,response){
+let app = http.createServer((request,response) => {
     let _url = request.url;
     let queryData = url.parse(_url, true).query;
-    let title = queryData.id
-    // console.log("queryData: ", queryData.id)
-    if(_url == '/'){
-        title = "Welcome"
-    }
-    if(_url == '/favicon.ico'){
-        return response.writeHead(404);
-    } 
-    response.writeHead(200);
-    fs.readFile(`data/${title}`, 'utf8', (err, description) => {
-        console.log(description)
-        let template = `
-        <!doctype html>
-        <html>
-        <head>
-            <title>WEB1 - ${title}</title>
-            <meta charset="utf-8">
-        </head>
-        <body>
-            <h1><a href="/">WEB</a></h1>
-        <ol>
-            <li><a href="/?id=HTML">HTML</a></li>
-            <li><a href="/?id=CSS">CSS</a></li>
-            <li><a href="/?id=JavaScript">JavaScript</a></li>
-        </ol>
-            <h2>${title}</h2>
-            <p>${description}</p>
-        </body>
-        </html>
-        `;
-        response.end(template); // 앞에서 작성한 코드를 무엇을 넣느냐에 따라 사용자에게 전송하는 데이터가 바뀐다./
-    })
-
-});
-app.listen(3000);
+    let pathname = url.parse(_url, true).pathname;
+    
+    if(pathname === '/'){
+        if(queryData.id === undefined){
+            fs.readFile(`data/${queryData.id}`, 'utf8', (err, description) => {
+                var title = 'Welcome'
+                var description = 'Hello, Node.js';
+                var template = `
+                <!doctype html>
+                <html>
+                <head>
+                    <title>WEB1 - ${title}</title>
+                    <meta charset="utf-8">
+                    </head>
+                    <body>
+                    <h1><a href="/">WEB</a></h1>
+                    <ol>
+                    <li><a href="/?id=HTML">HTML</a></li>
+                    <li><a href="/?id=CSS">CSS</a></li>
+                    <li><a href="/?id=JavaScript">JavaScript</a></li>
+                    </ol>
+                    <h2>${title}</h2>
+                    <p>${description}</p>
+                    </body>
+                    </html>
+                    `;
+                    response.writeHead(200);
+                    response.end(template);
+                });
+            }
+            else{
+                fs.readFile(`data/${queryData.id}`, 'utf8', (err, description) => {
+                    var title = queryData.id
+                    // console.log(description)
+                    let template = `
+                    <!doctype html>
+                    <html>
+                    <head>
+                    <title>WEB1 - ${title}</title>
+                    <meta charset="utf-8">
+                    </head>
+                    <body>
+                    <h1><a href="/">WEB</a></h1>
+                    <ol>
+                    <li><a href="/?id=HTML">HTML</a></li>
+                    <li><a href="/?id=CSS">CSS</a></li>
+                    <li><a href="/?id=JavaScript">JavaScript</a></li>
+                    </ol>
+                    <h2>${title}</h2>
+                    <p>${description}</p>
+                    </body>
+                    </html>
+                    `;
+                    response.writeHead(200);
+                    response.end(template); // 앞에서 작성한 코드를 무엇을 넣느냐에 따라 사용자에게 전송하는 데이터가 바뀐다./
+                });
+            }
+        }
+        else{
+            response.writeHead(404);
+            response.end('Not found');
+        }
+    });
+    app.listen(3000);
