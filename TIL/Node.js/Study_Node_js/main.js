bb368e2624414c1a262fe07cd78e2b7f3b993f39
Hwa Rang <snow2271@gmail.com>
2021-10-31 04:10:33 +0900

    함수를 활용한 코드 리팩토링

--- TIL/Node.js/Study_Node_js/main.js
+++ TIL/Node.js/Study_Node_js/main.js
@@ -2,38 +2,44 @@ let http = require('http');
 let fs = require('fs');
 let url = require('url');
 
+const templateHTML = (title, list, body) => {
+    return`
+    <!doctype html>
+    <html>
+    <head>
+    <title>WEB1 - ${title}</title>
+    <meta charset="utf-8">
+    </head>
+    <body>
+    <h1><a href="/">WEB</a></h1>
+    ${list}
+    ${body}
+    </body>
+    </html>
+    `;
+}
+
+const templateLIST = (fileList) => {
+    let list = '<ul>'
+    for(let i = 0; i < fileList.length; i++){
+        list = list + `<li><a href="/?id=${fileList[i]}">${fileList[i]}</a></li>`
+    }
+    list = list + '</ul>';
+    return list;
+}
+
 let app = http.createServer((request,response) => {
     let _url = request.url;
     let queryData = url.parse(_url, true).query;
     let pathname = url.parse(_url, true).pathname;
-    
+
     if(pathname === '/'){
         if(queryData.id === undefined){
             fs.readdir('./data', (err, fileList) => {
                 var title = 'Welcome'
                 var description = 'Hello, Node.js';
-
-                let list = '<ul>'
-                for(let i = 0; i < fileList.length; i++){
-                    list = list + `<li><a href="/?id=${fileList[i]}">${fileList[i]}</a></li>`
-                }
-                list = list + '</ul>';
-
-                var template = `
-                <!doctype html>
-                <html>
-                <head>
-                <title>WEB1 - ${title}</title>
-                <meta charset="utf-8">
-                </head>
-                <body>
-                <h1><a href="/">WEB</a></h1>
-                ${list}
-                <h2>${title}</h2>
-                <p>${description}</p>
-                </body>
-                </html>
-                `;
+                let list = templateLIST(fileList);
+                let template = templateHTML(title, list, `<h2>${title}</h2>${description}`)
                 response.writeHead(200);
                 response.end(template);
                 })
@@ -41,35 +47,11 @@ let app = http.createServer((request,response) => {
             else{
                 fs.readFile(`data/${queryData.id}`, 'utf8', (err, description) => {
                     fs.readdir('./data', (err, fileList) => {
-                        // var title = 'Welcome'
-                        // var description = 'Hello, Node.js';
-        
-                        let list = '<ul>'
-                        for(let i = 0; i < fileList.length; i++){
-                            list = list + `<li><a href="/?id=${fileList[i]}">${fileList[i]}</a></li>`
-                        }
-                        list = list + '</ul>';
-        
-        
+                    let list = templateLIST(fileList);
                     var title = queryData.id
-                    // console.log(description)
-                    let template = `
-                    <!doctype html>
-                    <html>
-                    <head>
-                    <title>WEB1 - ${title}</title>
-                    <meta charset="utf-8">
-                    </head>
-                    <body>
-                    <h1><a href="/">WEB</a></h1>
-                    ${list}
-                    <h2>${title}</h2>
-                    <p>${description}</p>
-                    </body>
-                    </html>
-                    `;
+                    let template = templateHTML(title, list, `<h2>${title}</h2>${description}`)
                     response.writeHead(200);
-                    response.end(template); // 앞에서 작성한 코드를 무엇을 넣느냐에 따라 사용자에게 전송하는 데이터가 바뀐다./
+                    response.end(template); // ! 앞에서 작성한 코드를 무엇을 넣느냐에 따라 사용자에게 전송하는 데이터가 바뀐다.
                 });
             });
         }
