fe7ffffc3bd6da7d711865bca55b027e04100a14
Hwa Rang <snow2271@gmail.com>
2021-11-09 23:54:47 +0900

    redux 도입으로인해 불필요해진 코드 삭제

--- TIL/React/redux/src/App.js
+++ TIL/React/redux/src/App.js
@@ -10,10 +10,8 @@ class App extends Component {
   return (
     <div className="App">
       <h1>Root</h1>
-      <AddNumberRoot onClick={(size) => {
-        this.setState({number: this.state.number + size});
-      }}/>
-      <DisplayNumberRoot number={this.state.number}/>
+      <AddNumberRoot />
+      <DisplayNumberRoot />
     </div>
     )
   }
