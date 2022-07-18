008ab3f19c12dfb783f74286389db4c88df9eea4
LeeHwaRang <snow2271@gmail.com>
2022-07-18 21:26:43 +0900

    도커란?

--- golang/hello/hello.go
+++ golang/hello/hello.go
@@ -2,5 +2,22 @@ package main
 
 import "fmt"
 func main() {
-	fmt.Println("hello world")
+	// hello wordl 띄우기
+	// fmt.Println("hello world")
+
+	/*
+	변수 선언
+	var a int = 0
+	var message string = ""
+	
+	a=20
+	message = "good morning"
+	
+	fmt.Println(message, a)
+	*/
+	var min int = 10
+	var work int = 20
+
+	var result int = min * work
+	fmt.Print(min, work, result)
 }
